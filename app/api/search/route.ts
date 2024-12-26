5893dfb78a261a58c996c4ad278714908a4d3bbd
“rjjan” <ryan.rj.jan@gmail.com>
2024-12-26 10:51:58 +0800

    test

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,4 +1,22 @@
-import { source } from '@/lib/source';
-import { createFromSource } from 'fumadocs-core/search/server';
+import { source } from "@/lib/source";
+import { createFromSource } from "fumadocs-core/search/server";
+// @ts-expect-error -- untyped
+import { createTokenizer } from "@orama/tokenizers/mandarin";
+import { stopwords as mandarinStopwords } from "@orama/stopwords/mandarin";
+import { cn } from "fumadocs-ui/components/api";
 
-export const { GET } = createFromSource(source);
+export const { GET, search } = createFromSource(source, undefined, {
+  localeMap: {
+    // the prop name should be its locale code in your i18n config, (e.g. `cn`)
+    "zh-TW": {
+      // options for the language
+      tokenizer: await createTokenizer({
+        stopWords: mandarinStopwords,
+      }),
+      search: {
+        threshold: 0.1,
+        tolerance: 1,
+      },
+    },
+  },
+});
--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,17 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
+/* https://nextjs.org/docs/pages/building-your-application/routing/internationalization */
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  i18n: {
+    // Supported locales
+    locales: ["en-US", "zh-TW"],
+    // Default locale
+    defaultLocale: "zh-TW",
+  },
 };
 
 export default withMDX(config);
